c9addd58c6b595f01aa409c811bc6ce0ae300acc
Nick O'Neill <nick.oneill@gmail.com>
2024-01-25 22:17:34 -0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/nickoneill/office-finder
+
+go 1.21.5
+
+require github.com/mauidude/go-readability v0.0.0-20220221173116-a9b3620098b7
+
+require (
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/joho/godotenv v1.5.1
+	github.com/microcosm-cc/bluemonday v1.0.26
+	github.com/sashabaranov/go-openai v1.18.3
+	golang.org/x/net v0.17.0 // indirect
+	jaytaylor.com/html2text v0.0.0-20230321000545-74c2419ad056
+)
---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/sashabaranov/go-openai"
+	"jaytaylor.com/html2text"
+)
+
+func main() {
+	res, err := findAddresses("https://www.lujan.senate.gov")
+	if err != nil {
+		log.Fatalf("some error: %s", err)
+	}
+
+	log.Println(res)
+}
+
+func findAddresses(contentURL string) (string, error) {
+	_, err := url.ParseRequestURI(contentURL)
+	if err != nil {
+		return "", err
+	}
+
+	res, err := http.Get(contentURL)
+	if err != nil {
+		return "", err
+	}
+
+	if res.StatusCode != 200 {
+		return "", fmt.Errorf("status code %d for url %s", res.StatusCode, contentURL)
+	}
+
+	html, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		return "", err
+	}
+
+	// readability will try to find the main content of a url, it's
+	// not perfect but it does a good enough job for the summarize request
+	// doc, err := readability.NewDocument(string(html))
+	// if err != nil {
+	// 	return "", err
+	// }
+
+	// content := doc.Content()
+	// log.Printf("content was: %s", content)
+	// // readability leaves HTML in place so we need to strip all tags too
+	// p := bluemonday.StripTagsPolicy()
+	// nonhtml := p.Sanitize(content)
+
+	htmlText, err := html2text.FromString(string(html), html2text.Options{TextOnly: true})
+	if err != nil {
+		return "", fmt.Errorf("can't parse html to string: %s", err)
+	}
+
+	// log.Printf("non-html content: %s", htmlText)
+	// return "", fmt.Errorf("error")
+
+	openaiToken := os.Getenv("OPENAI_API_KEY")
+	if openaiToken == "" {
+		return "", fmt.Errorf("no openai token found")
+	}
+
+	client := openai.NewClient(openaiToken)
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT4TurboPreview,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleSystem,
+					Content: `you are an assistant designed to extract addresses from text content, returning them in a json format which includes the fields: address, city, state, zip, phone`,
+				},
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: addressPrompt(htmlText),
+				},
+			},
+		},
+	)
+	if err != nil {
+		return "", err
+	}
+
+	return resp.Choices[0].Message.Content, nil
+}
+
+func addressPrompt(content string) string {
+	prompt := fmt.Sprintf("find the addresses in this content: %s", content)
+	// log.Println(prompt)
+
+	return prompt
+}
